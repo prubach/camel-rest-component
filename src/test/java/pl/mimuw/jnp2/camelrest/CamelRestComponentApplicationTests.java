07fc7caae4484b317b88c635e50a70ecc005b7c0
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-04-23 12:39:57 +0200

    Added example junit test

--- src/test/java/pl/mimuw/jnp2/camelrest/CamelRestComponentApplicationTests.java
+++ src/test/java/pl/mimuw/jnp2/camelrest/CamelRestComponentApplicationTests.java
@@ -1,5 +1,6 @@
 package pl.mimuw.jnp2.camelrest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,10 @@ class CamelRestComponentApplicationTests {
 
     @Test
     void contextLoads() {
+        int a  = 5;
+        int b = 6;
+        // Uncomment below and run with "gradle test"
+        //assertEquals(20, a+b,"Nie działa");
     }
 
 }
